52ad7c980ff674e57cfe6478f1724e36ddec0637
iyed-benali <aveyrokepler86@gmail.com>
2023-10-06 10:32:58 +0100

    compiling

---
+++ sandbox.js
@@ -0,0 +1,7 @@
+var character = "mario";
+console.log(character);
+var inputs = document.querySelectorAll('input');
+console.log(inputs);
+inputs.forEach(function (element) {
+    console.log(element);
+});
