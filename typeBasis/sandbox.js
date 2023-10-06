5c96da24c499fd1fab1fae2a11aba1e9971b7bbc
iyed-benali <aveyrokepler86@gmail.com>
2023-10-06 10:42:39 +0100

    done

---
+++ typeBasis/sandbox.js
@@ -0,0 +1,7 @@
+var boolean = true;
+var a = "ahmed";
+var num = 3;
+var circle = function (diameter) {
+    return Math.PI * diameter;
+};
+console.log(circle(22));
---
+++ typeBasis/sandbox.ts
@@ -0,0 +1,13 @@
+var boolean = true
+
+var a = "ahmed"
+
+var num = 3 
+
+
+
+const circle = function(diameter : number){
+return Math.PI * diameter
+}
+
+console.log(circle(22))
