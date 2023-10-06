794a513c9f35793586ace0b2ca4f1123a8b49b6b
iyed-benali <aveyrokepler86@gmail.com>
2023-10-06 11:47:26 +0100

    de

---
+++ dynamicDataTypes/sandbox.js
@@ -0,0 +1,13 @@
+var age;
+age = 25;
+age = "ahmed";
+age = true;
+var arr = [];
+arr.push("ahmed");
+arr.push(3);
+arr.push(true);
+var obj;
+obj = {
+    name: 23,
+    age: "ahmed",
+};
---
+++ dynamicDataTypes/sandbox.ts
@@ -0,0 +1,24 @@
+let age : any
+
+age = 25
+
+age = "ahmed"
+
+age = true
+
+
+let arr : any[] = []
+
+arr.push("ahmed")
+arr.push(3)
+arr.push(true)
+
+let obj : {
+    name: any,
+    age : any
+}
+
+obj = {
+    name: 23,
+    age:"ahmed",
+}
