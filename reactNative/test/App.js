97297c4872dd20ee7f3d26c46b3bd6ab23ae76b9
iyed-benali <aveyrokepler86@gmail.com>
2023-12-31 12:50:06 +0100

    dd

---
+++ reactNative/test/App.js
@@ -0,0 +1,40 @@
+import { StatusBar } from 'expo-status-bar';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import React ,{useState} from 'react';
+import app from './firebase';
+import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
+export default function App() {
+  
+
+  const [email,setEmail] = useState('')
+  const [password , setPassword] = useState('')
+  const auth = getAuth()
+const handleRegister = async ()=>{
+try{
+  const userCred = await createUserWithEmailAndPassword(auth,email,password)
+var user =  userCred.user
+console.log(user,'gggg')
+}
+catch(err){
+  console.log(err)
+}
+}
+  return (
+    <View style={styles.container}>
+     <TextInput placeholder='email' value={email} onChangeText={setEmail} >
+     </TextInput>
+     <TextInput placeholder='password' value={password} onChangeText={setPassword}  secureTextEntry>
+     </TextInput>
+     <Button title = 'register' onPress={handleRegister}></Button>
+    </View>
+  );
+} 
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ reactNative/test/firebase.js
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp  } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+console.log('nicee')
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCujYDC4RBSsnd_cjvpDoPJ11-fq_MmWVI",
+  authDomain: "test-d940b.firebaseapp.com",
+  projectId: "test-d940b",
+  storageBucket: "test-d940b.appspot.com",
+  messagingSenderId: "1066928509398",
+  appId: "1:1066928509398:web:33416670269e6ec4d6d477",
+  measurementId: "G-P1QHLY0HFS"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+export default app;
+
